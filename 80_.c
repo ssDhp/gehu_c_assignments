629fc624464ecce81e3ee2204bca1ea080fb6e2c
Axel Windows <110320550+ssDhp@users.noreply.github.com>
2022-11-02 16:37:01 +0530

    Added 79 - 80

--- 80_.c
+++ 80_.c
@@ -1,5 +1,4 @@
-// Write a C Program to print the upper and lower triangle in a matrix
-
+// Write a c Program to find addition of two 2d array
 #include <stdio.h>
 
 void printMatrix(int row, int col, int arr[row][col])
@@ -26,45 +25,42 @@ void main()
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &x1, &y1);
     int arr1[x1][y1];
+    int arr2[x1][y1];
+    // Array 1
+    printf("\nFor Array 1: \n");
     for (i = 0; i < x1; i++)
     {
         for (j = 0; j < y1; j++)
         {
             int a = 0;
-            printf("Enter element at [%d][%d]: ", i, j);
+            printf("\t Enter element at [%d][%d]: ", i, j);
             scanf("%d", &a);
             arr1[i][j] = a;
         }
     }
-    printf("\nArray: ");
+    printf("\nArray 1: ");
     printMatrix(x1, y1, arr1);
-    // Upper triangluar matrix
-    printf("Upper Triangluar matrix:\n\t");
+    // Array 2
+    printf("For Array 2: \n");
     for (i = 0; i < x1; i++)
     {
         for (j = 0; j < y1; j++)
         {
-            if (i <= j)
-                printf("%d", arr1[i][j]);
-            else
-                printf("%d", 0);
-            if (j == y1 - 1)
-                printf("\n\t");
-            else
-                printf(", ");
+            int a = 0;
+            printf("\t Enter element at [%d][%d]: ", i, j);
+            scanf("%d", &a);
+            arr2[i][j] = a;
         }
     }
-    printf("\n");
-    // Lower triangle matrix
-    printf("Lower Triangluar matrix:\n\t");
+    printf("\nArray 2: ");
+    printMatrix(x1, y1, arr2);
+    // Sum of Array 1 and Array 2
+    printf("Sum: \n\t");
     for (i = 0; i < x1; i++)
     {
         for (j = 0; j < y1; j++)
         {
-            if (i >= j)
-                printf("%d", arr1[i][j]);
-            else
-                printf("%d", 0);
+            printf("%d", arr1[i][j] + arr2[i][j]);
             if (j == y1 - 1)
                 printf("\n\t");
             else
